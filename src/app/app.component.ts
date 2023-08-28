324b2bb0bef41d812f68df23896c7d4636842448
Albert-William Lemieux <1565864@cegepmontpetit.ca>
2023-08-28 10:47:31 -0400

    complété
    labo complété

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'laboratoire1';
+  artistes: Artiste[] = [];
+
+  a: string = "";
+  
+  constructor(){
+    this.artistes = [new Artiste("Gentle Giant"),
+                      new Artiste("Genesis"),
+                        new Artiste("Yes")];
+  }
+
+  AddArtiste() : void{
+  this.artistes.push(new Artiste(this.a));
+  }
+
 }
+
+class Artiste {
+
+  constructor(public name: string){
+
+  }
+}
+
